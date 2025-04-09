dfb620a3a06f8e11212785212aa0fb9b4c2ad28c
Md Zeyad Ali <91756868+mdzeyad29@users.noreply.github.com>
2025-04-09 14:31:06 +0530

    Add files via upload

---
+++ Routing/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import { Navbar } from './component/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+
+function App() {
+  return (
+   <div>
+   <Navbar/>
+   <Routes>
+   <Route path='/' element={<Home/>}/> 
+   <Route path='/about' element={<About/>}/>
+   <Route path='/contact' element={<Contact/>}/>
+   </Routes>
+   </div>
+  );
+}
+
+export default App;
